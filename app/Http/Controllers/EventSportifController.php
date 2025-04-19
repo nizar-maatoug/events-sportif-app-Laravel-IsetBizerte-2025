4892234526e667f81c6d6abba456ad34efd7e259
Nizar MAATOUG <nizar.maatoug@hotmail.com>
2025-04-19 07:57:17 +0100

    add EventSportifControllers

---
+++ app/Http/Controllers/EventSportifController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EventSportif;
+use Illuminate\Http\Request;
+
+class EventSportifController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(EventSportif $eventSportif)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(EventSportif $eventSportif)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, EventSportif $eventSportif)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(EventSportif $eventSportif)
+    {
+        //
+    }
+}
