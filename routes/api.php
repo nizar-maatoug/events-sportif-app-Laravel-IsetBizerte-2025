81614354864557b466576477e3ddf78a3419962b
Nizar MAATOUG <nizar.maatoug@hotmail.com>
2025-04-25 10:28:28 +0100

    install api features

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
-
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventSportifController;
-use Illuminate\Routing\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::prefix('v1')->group(function () {
     // EventSportif API Resource Routes
