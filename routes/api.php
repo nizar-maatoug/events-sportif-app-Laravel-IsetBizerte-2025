00104f8f1dc7cf15809c39e6b9046944821bb520
Nizar MAATOUG <nizar.maatoug@hotmail.com>
2025-05-03 05:22:56 +0100

    add auth routes

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,13 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\EventSportifController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
 Route::prefix('v1')->group(function () {
     // EventSportif API Resource Routes
