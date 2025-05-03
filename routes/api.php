d80cfa0e40531dadf1f8778b3360cf8ba06e55e3
Nizar MAATOUG <nizar.maatoug@hotmail.com>
2025-05-03 07:56:04 +0100

    add protected routes for eventSportif

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,7 @@ use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\EventSportifController;
-
-
+use App\Http\Middleware\CheckRole;
 
 Route::prefix('v1')->group(function () {
     // EventSportif API Resource Routes
@@ -13,7 +12,52 @@ Route::prefix('v1')->group(function () {
 
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
-    Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+    // Public routes
+    Route::get('/events', [EventSportifController::class, 'index'])
+    ->name('events.index');
+    // Route to get all events
+    Route::get('/events/{eventSportif}', [EventSportifController::class, 'show'])
+    ->name('events.show');
+
+    // Protected routes
+    // Authenticated user routes
+    // These routes require authentication and are protected by the 'auth:sanctum' middleware
+    // and the CheckRole middleware to check user roles.
+    // The CheckRole middleware checks if the authenticated user has the specified role
+    // before allowing access to the route.
+    // The routes are grouped together for better organization and readability.
+    // The 'auth:sanctum' middleware is used to protect the routes that require authentication.
+    // The CheckRole middleware is used to check the user's role for specific routes.
+    Route::middleware('auth:sanctum')->group(function(){
+        Route::post('/logout', [AuthController::class, 'logout']);
+
+        // EventSportif API Resource Routes
+        Route::post('/events', [EventSportifController::class, 'store'])
+        ->middleware(CheckRole::class,':admin,organizer')->name('events.store');
+
+        Route::put('/events/{eventSportif}', [EventSportifController::class, 'update'])
+        ->middleware('can:update,eventSportif')->name('events.update');
+
+        Route::delete('/events/{eventSportif}', [EventSportifController::class, 'destroy'])
+        ->middleware('can:delete,eventSportif')->name('events.destroy');
+
+        // Organizer-only routes
+        Route::middleware(CheckRole::class,':organizer')->group(function () {
+            Route::get('/organizer/events', [EventSportifController::class, 'organizerEvents'])
+            ->name('organizer.events.index');
+        });
+
+        // Admin-only routes
+        Route::middleware(CheckRole::class,':admin')->group(function () {
+            Route::get('/admin/stats', [EventSportifController::class, 'adminStats'])
+            ->name('admin.events.stats');
+        });
+
+
+
+
+    });
 
 
     });
