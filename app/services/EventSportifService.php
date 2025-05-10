c91e3372c1489f919da7c5f8a07a70a63a481b69
Nizar MAATOUG <nizar.maatoug@hotmail.com>
2025-05-10 08:11:50 +0100

    update EventSportifService

--- app/services/EventSportifService.php
+++ app/services/EventSportifService.php
@@ -2,8 +2,10 @@
 
 namespace App\Services;
 use App\Models\EventSportif;
+use App\Models\Photo;
 use App\Services\Interfaces\EventSportifServiceInterface;
 use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Validation\Rule;
@@ -11,32 +13,69 @@ use Illuminate\Validation\Rule;
 
 class EventSportifService implements EventSportifServiceInterface
 {
+
+
+    public function __construct(private PhotoService $photoService)
+    {
+        // Constructor code if needed
+    }
+
     public function getAllEvents($perPage = 10):Paginator
     {
-        return EventSportif::paginate($perPage);
+        return EventSportif::with(['poster','logo'])->paginate($perPage);
     }
 
     public function getEventById(int $id): ?EventSportif
     {
-        return EventSportif::find($id);
+        return EventSportif::find($id)->with(['poster','logo']);
     }
 
     public function createEvent(array $data): EventSportif
     {
         // Validate the data
         $data = $this->validateEventData($data);
+        DB::beginTransaction();
         // Create the event
-        return EventSportif::create($data);
+        $eventSportif= EventSportif::create($data);
+        // Handle the photos
+        if (isset($data['poster'])) {
+            $poster = $this->photoService->uploadPhoto($data['poster'],get_class($eventSportif),$eventSportif->id, 'poster', 'photos/events/posters');
+            $eventSportif->poster()->associate($poster);
+        }
+        if (isset($data['logo'])) {
+            $logo = $this->photoService->uploadPhoto($data['logo'],get_class($eventSportif),$eventSportif->id, 'logo', 'photos/events/logos');
+            // Associate the logo with the event
+            $eventSportif->logo()->associate($logo);
+        }
+        // Save the event with the associated photos
+        $eventSportif->save();
+
+        // Commit the transaction
+        DB::commit();
+        return $eventSportif;
     }
 
     public function updateEvent(int $id, array $data): ?EventSportif
     {
         // Validate the data
         $data = $this->validateEventData($data);
+
         // Find and update the event
         $event = EventSportif::findOrFail($id);
         if ($event) {
+            DB::beginTransaction();
             $event->update($data);
+            // Handle the photos
+            if (isset($data['poster'])) {
+                $poster = $this->photoService->updatePhoto($data['poster'], $event->poster->id, 'photos/events/posters');
+                $event->poster()->associate($poster);
+            }
+            if (isset($data['logo'])) {
+                $logo = $this->photoService->updatePhoto($data['logo'], $event->logo->id, 'photos/events/logos');
+                // Associate the logo with the event
+                $event->logo()->associate($logo);
+            }
+            DB::commit();
             return $event;
         }
         return null;
@@ -46,7 +85,17 @@ class EventSportifService implements EventSportifServiceInterface
     {
         $event = EventSportif::find($id);
         if ($event) {
-            return $event->delete();
+            DB::beginTransaction();
+            $event->delete();
+            // Delete the associated photos
+            if ($event->poster) {
+                $this->photoService->deletePhoto($event->poster->id);
+            }
+            if ($event->logo) {
+                $this->photoService->deletePhoto($event->logo->id);
+            }
+            DB::commit();
+            return true;
         }
         return false;
     }
